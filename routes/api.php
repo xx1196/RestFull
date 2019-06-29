3bec3a604a6650fa2f74d2f3a5b2b7f2fd0cd06c
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-06-28 20:50:27 -0500

    Creando las rutas basicas del proyecto

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,39 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::resource('buyers', 'buyer\BuyerController',
+    [
+        'only' => ['index', 'show']
+    ]
+);
+
+Route::resource('categories', 'category\CategoryController',
+    [
+        'except' => ['create', 'edit']
+    ]
+);
+
+Route::resource('products', 'product\ProductController',
+    [
+        'only' => ['index', 'show']
+    ]
+);
+
+Route::resource('transactions', 'transaction\TransactionController',
+    [
+        'only' => ['index', 'show']
+    ]
+);
+
+Route::resource('sellers', 'seller\SellerController',
+    [
+        'only' => ['index', 'show']
+    ]
+);
+
+Route::resource('users', 'user\UserController',
+    [
+        'except' => ['create', 'edit']
+    ]
+);
