088e592153e5e3f978270f0b573d12207f2b49a7
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-06-29 22:52:13 -0500

    Implementando propiedades del modelo de productos

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /*
+     * Constante de estados
+     */
+    const PRODUCT_AVAILABLE = 1;
+    const PRODUCT_NOT_AVAILABLE = 0;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'seller_id',
+    ];
+
+    public function isAvailable()
+    {
+        return $this->state == self::PRODUCT_AVAILABLE;
+    }
 }
