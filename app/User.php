f93779335b11558c8dff1178d6bec914531be12d
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-06-29 23:08:02 -0500

    Implementando propiedades del modelo de usuario

--- app/User.php
+++ app/User.php
@@ -10,13 +10,26 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    /*
+     * Constantes de estado
+     */
+    const USER_VERIFIED = '1';
+    const USER_NOT_VERIFIED = '0';
+    const USER_ADMIN = '1';
+    const USER_REGULAR = '0';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verified_token',
+        'admin',
     ];
 
     /**
@@ -25,7 +38,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'verified_token',
     ];
 
     /**
@@ -36,4 +51,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isVerified()
+    {
+        return $this->verified == self::USER_VERIFIED;
+    }
+
+    public function isAdmin()
+    {
+        return $this->verified == self::USER_VERIFIED;
+    }
+
+    public static function generateVerifiedToken()
+    {
+        return str_random(40);
+    }
 }
