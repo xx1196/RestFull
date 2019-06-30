aefcce3e01eabb9e9efe0ddc9ae590dab3966c13
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-06-29 23:26:47 -0500

    Modificando migracion de usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\User;
 
 class CreateUsersTable extends Migration
 {
@@ -17,9 +18,11 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('verified')->default(User::USER_NOT_VERIFIED);
+            $table->string('verified_token')->nullable();
+            $table->string('admin')->default(User::USER_REGULAR);
             $table->timestamps();
         });
     }
