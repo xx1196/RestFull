072b890d6e360bae89b7bcecdae49c5459226dbc
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-07-14 17:35:01 -0500

    Agregando Factory de categorias

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $factory Factory */
+
+use App\Category;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
