8326814c0b45438cb6bd410341feea12b3bcc160
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-07-14 18:22:35 -0500

    Resolviendo error de llaves foraneas

--- database/migrations/2019_06_29_012653_create_products_table.php
+++ database/migrations/2019_06_29_012653_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
             $table->string('description', 1000);
             $table->unsignedInteger('quantity');
--- database/migrations/2019_06_29_012924_create_categories_table.php
+++ database/migrations/2019_06_29_012924_create_categories_table.php
@@ -14,7 +14,7 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
             $table->string('description', 1000);
             $table->timestamps();
