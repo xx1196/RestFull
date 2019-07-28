1ca2b85022d14cb5b49c397f7914e6f551726937
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-07-28 02:07:03 -0500

    implementando operaciones para productos

--- app/Http/Controllers/product/ProductController.php
+++ app/Http/Controllers/product/ProductController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\product;
 
 use App\Http\Controllers\ApiController;
 use App\Product;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class ProductController extends ApiController
 {
@@ -16,28 +14,8 @@ class ProductController extends ApiController
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
+        $products = Product::all();
+        return $this->showAll($products);
     }
 
     /**
@@ -48,40 +26,6 @@ class ProductController extends ApiController
      */
     public function show(Product $product)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param Product $product
-     * @return void
-     */
-    public function update(Request $request, Product $product)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param Product $product
-     * @return void
-     */
-    public function destroy(Product $product)
-    {
-        //
+        return $this->showOne($product);
     }
 }
