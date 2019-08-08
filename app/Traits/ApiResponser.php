bcfccfdd91b716d5d3d7afd31c3a264a9af3c33b
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-08-07 21:16:13 -0500

    Paginando resultados de colecciones

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,7 +3,9 @@
 namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
 {
@@ -30,8 +32,11 @@ trait ApiResponser
 
     protected function showAll(Collection $collection, $code = 200)
     {
+
+        $collection = $this->paginate($collection);
+
         return $this->successResponse([
-            'data' => $collection
+            $collection
         ],
             $code
         );
@@ -65,4 +70,33 @@ trait ApiResponser
         );
     }
 
+    protected function paginate(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50'
+        ];
+
+        $messages = [
+            'per_page.integer' => 'El valor por página debe ser un valor entero',
+            'per_page.min' => 'El valor por página debe ser mayor a 1',
+            'per_page.max' => 'El valor por página debe ser menor a 50',
+        ];
+
+        Validator::validate(request()->all(), $rules, $messages);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 15;
+
+        if (request()->has('per_page'))
+            $perPage = (int)request()->per_page;
+
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+
+        $paginated->appends(request()->all());
+        return $paginated;
+    }
 }
